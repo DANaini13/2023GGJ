aa6d9b95ca24526f1c8cdb36f5613d111886e1a0
YaNgYu <75065276+YaN9Yu96@users.noreply.github.com>
2023-02-04 01:23:17 +0800

    彻底拔出来的演出

---
+++ Assets/Yang/CommentBar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class CommentBar : MonoBehaviour
+{
+    public Text text;
+    public void Born(float delay, string str)
+    {
+        text.text = str;
+        this.transform.localScale = Vector3.zero;
+        this.transform.DOScale(Vector3.one, 0.3f).SetEase(Ease.InCubic).SetDelay(delay);
+    }
+}
